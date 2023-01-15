8123656b32d1b0ec6484a180289a9cc41fa3831e
Vladyslav Tarasov <vladyslavtarasov02@gmail.com>
2023-01-15 20:11:24 +0200

    Add JWTManager and error handlers

--- backend_labs/views.py
+++ backend_labs/views.py
@@ -3,6 +3,7 @@ import os
 from backend_labs import app
 from flask_smorest import Api #type:ignore
 from flask_jwt_extended import JWTManager #type:ignore
+from flask import jsonify #type:ignore
 
 from backend_labs.data import db
 
@@ -27,6 +28,34 @@ api = Api(app)
 db.init_app(app)
 
 jwt = JWTManager(app)
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+   return (
+       jsonify({"message": "The token has expired.", "error": "token_expired"}),
+       401,
+   )
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+   return (
+       jsonify(
+           {"message": "Signature verification failed.", "error": "invalid_token"}
+       ),
+       401,
+   )
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+   return (
+       jsonify(
+           {
+               "description": "Request does not contain an access token.",
+               "error": "authorization_required",
+           }
+       ),
+       401,
+   )
 
 with app.app_context():
     db.create_all()
